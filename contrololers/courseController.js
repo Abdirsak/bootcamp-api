0fe96e96b96e4d74907422db23ef18586681e4cb
abdirizak <mygithubemail26@gmail.com>
2023-06-12 10:15:07 +0300

    add course and single course, update and delete course

--- contrololers/courseController.js
+++ contrololers/courseController.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/ErrorResponse')
-const Course = require('../models/Course')
+const Course= require('../models/Course')
+const Bootcamp = require('../models/Bootcamp')
 const asyncHandler = require('../middleware/assync')
 
 exports.getCourse = asyncHandler(async (req , res , next)=>{
@@ -7,7 +8,7 @@ exports.getCourse = asyncHandler(async (req , res , next)=>{
     if(req.params.bootcampId){
         query = Course.find({bootcamp : req.params.bootcampId})
     }else{
-        query = Course.find().populate('bootcamp', "name website")
+        query = Course.find().populate('bootcamp', "name description")
     }
     const courses = await query;
     res.status(200).send({
@@ -16,4 +17,57 @@ exports.getCourse = asyncHandler(async (req , res , next)=>{
         data : courses
     })
 
+})
+
+exports.getSingleCourse = asyncHandler(async (req, res, next)=>{
+    const course = await Course.findById(req.params.id).populate('bootcamp' , 'name description')
+
+    if(!course){
+        return next(new ErrorResponse(`no course with the id of ${req.params.id}`),404)
+    }
+
+    res.status(200).send({
+        success : true,
+        data : course
+    })
+})
+
+exports.addCourse = asyncHandler(async (req , res , next)=>{
+    req.body.bootcamp = req.params.bootcampId
+
+    const getBootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+    if(!getBootcamp){
+        return next(new ErrorResponse(`no bootcamp with the id of ${req.params.bootcampId}`), 404)
+    }
+
+    const course = Course.create(req.body)
+
+    res.status(200).send({
+        success : true,
+        data : course
+    })
+})
+
+
+exports.updateCourse = asyncHandler (async (req, res, next)=>{
+    const response = await Course.findByIdAndUpdate(req.params.id , req.body, {new : true})
+
+    if(!response){
+        return next(new ErrorResponse(`no course with the id of ${req.params.id}`), 404)
+    }
+
+    res.status(200).send({
+        success : true,
+        data : response
+    })
+})
+
+exports.deleteCourse = asyncHandler (async(req , res , next)=>{
+    const deletedbootcamps = await Course.findById(req.params.id)
+    if(!deletedbootcamps){
+    return next(new ErrorResponse(`not found id:${req.params.id}`))
+}
+deletedbootcamps.deleteOne()
+res.send({success: true , msg : `Delete course ${req.params.id}`})
 })
--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const {getCourse
-} = require('../contrololers/courseController')
+const {getCourse , getSingleCourse , addCourse,updateCourse, deleteCourse} = require('../contrololers/courseController')
 
 const courseRouter = express.Router({mergeParams : true})
 
-courseRouter.route('/').get(getCourse)
+courseRouter.route('/').get(getCourse).post(addCourse)
+courseRouter.route('/:id').get(getSingleCourse).put(updateCourse).delete(deleteCourse)
 
 module.exports = courseRouter
